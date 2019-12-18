dfd3ed46ce02c630a869909b8e68d3bc5252e626
Chris Brody <chrisbrody@codifyacademy.com>
2019-12-18 09:18:23 -0500

    Updates

---
+++ precache-manifest.c5d9a7d74944d7f482cd9ca772762599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724bf756289f044155f6e11651b8e878",
+    "url": "/project-react-js-budget-calculator/index.html"
+  },
+  {
+    "revision": "67fab1d3d63ca10641b9",
+    "url": "/project-react-js-budget-calculator/static/css/main.8b0a52b7.chunk.css"
+  },
+  {
+    "revision": "cc70fce402425b7db718",
+    "url": "/project-react-js-budget-calculator/static/js/2.b62e167a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project-react-js-budget-calculator/static/js/2.b62e167a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67fab1d3d63ca10641b9",
+    "url": "/project-react-js-budget-calculator/static/js/main.9d65b319.chunk.js"
+  },
+  {
+    "revision": "e1fd19d9f11b3701ea85",
+    "url": "/project-react-js-budget-calculator/static/js/runtime-main.922528a1.js"
+  }
+]);
